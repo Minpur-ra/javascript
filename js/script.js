0948946536d873b49ba4ab892c05809fed893552
Minpur-ra <jordankarina46@gmail.com>
2025-08-12 16:07:10 -0300

    first impressions
    inicio javascript

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+console.log("help")
+
+
+let a = 9
+console.log("a é: " + a)
+let b = 2
+console.log("b é: "+ b)
+console.log( a + b)
+
+{
+    {
+        {
+            var m = 4
+        }
+        console.log(m+1)
+    }
+}
+
