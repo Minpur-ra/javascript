a2af200e70ff8d09fc96874d7ae3a63e6698f0cc
Minpur-ra <jordankarina46@gmail.com>
2025-09-04 01:37:29 -0300

    atualização
    arrumei

--- programação/q6.js
+++ programação/q6.js
@@ -1,6 +1,34 @@
 
-while(true){
-    
-    let aluno = 5
 
+
+
+let alunos = []
+
+let media = 0, soma = 0
+
+i=0
+
+while(i<5){
+    
+    let matricula = Number(prompt("Digite a matrícula do aluno :"))
+    i++
+    let nota = Number(prompt("Digite a nota do aluno :"))
+    alunos.push({matricula, nota})
+    soma = soma + nota
     }
+    media = soma / i
+    alert(" a média da turma é: "+ media)
+
+    let passou = alunos.filter(aluno=> aluno.nota > media)
+
+    if(passou.length > 0){
+        let resultado = "Os alunos acima da média são: "
+            passou.forEach(aluno => {
+                resultado += "matricula: "+ aluno.matricula +" "
+    })
+alert(resultado)}
+    else{
+        alert("Nenhum aluno está acima da média")
+    }
+
+    
