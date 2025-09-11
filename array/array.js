8bec2cf281a68348d4f4dd70f01755ce6140ad89
Minpur-ra <jordankarina46@gmail.com>
2025-09-11 15:07:39 -0300

    array
    sjsjsjsjs

---
+++ array/array.js
@@ -0,0 +1,10 @@
+let car ={
+    nome: "soasd",
+    ano: 1223
+}
+
+
+let carro = ['vivo', 'sans', 'steys']  //ainda se em branco o array aparece, ainda que vazio
+
+
+let ser = [ 'ana', 'sapatona', 'lygia', 'doce']
