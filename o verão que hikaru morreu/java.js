18ddcfba75dca47a7219557704693d1025979be9
Minpur-ra <jordankarina46@gmail.com>
2025-10-09 04:44:49 -0300

    semitermino
    scrro

---
+++ o verão que hikaru morreu/java.js
@@ -0,0 +1,34 @@
+
+
+
+
+function inf1(){
+    const inf = document.getElementById("inf")
+    if (inf.style.display === "none"){
+        inf.style.display = "block"
+    }else{
+        inf.style.display = "none"}
+}
+function inf2(){
+    const inf2 = document.getElementById("inf2")
+        if (inf2.style.display === "none"){
+            inf2.style.display = "block"
+        }else{
+            inf2.style.display = "none"}
+}
+function inf3(){
+    const inf3 = document.getElementById("inf3")
+    if(inf3.style.display === "none"){
+        inf3.style.display = "block"
+    }else{
+        inf3.style.display = "none"}
+}
+function inf4(){
+    const inf4 = document.getElementById("inf4")
+    if(inf4.style.display === "none"){
+        inf4.style.display = "block"
+    }
+    else{
+        inf4.style.display = "none"
+    }
+}
