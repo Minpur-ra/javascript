38fb619e3c603b3f24d6c8796243bbddf7499971
Minpur-ra <jordankarina46@gmail.com>
2025-10-09 14:59:32 -0300

    att
    ewfdfafa

--- o verão que hikaru morreu/java.js
+++ o verão que hikaru morreu/java.js
@@ -1,6 +1,9 @@
 
 
 
+    var x = document.getElementById("musica")
+    x.play()
+
 
 function inf1(){
     const inf = document.getElementById("inf")
@@ -30,5 +33,14 @@ function inf4(){
     }
     else{
         inf4.style.display = "none"
+    }
+}
+function inf5(){
+    const inf5 = document.getElementById("inf5")
+    if(inf5.style.display === "none"){
+        inf5.style.display = "block"
+    }
+    else{
+        inf5.style.display = "none"
     }
 }
